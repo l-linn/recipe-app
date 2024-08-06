ff1e1d12b9437d9d002f602844d8d30102d50847
lin <lin7cb@outlook.com>
2024-08-06 18:09:19 +0100

    exercise2.4

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# The render function requires two arguments ( request and template) and returns an HttpResponse object with that rendered text.
+def recipes_home(request):
+    return render(request, "recipes/recipes_home.html")
