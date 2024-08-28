d64899cd982c2e3a860b09ba153af732bc1d2067
lin <lin7cb@outlook.com>
2024-08-28 22:22:06 +0100

    Update prod.py

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -40,7 +40,7 @@ DATABASES = {
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES["default"].update(db_from_env)
