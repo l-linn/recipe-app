be7e1515acba8305a910ec51c7a1d42120621a3d
lin <lin7cb@outlook.com>
2024-08-28 22:21:08 +0100

    Update prod.py

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -42,8 +42,8 @@ DATABASES = {
 # Heroku: Update database configuration from $DATABASE_URL.
 # import dj_database_url
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES["default"].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 CORS_ORIGIN_ALLOW_ALL = True
 
